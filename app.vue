a9bf285bc8f496f9284483419f535aa852438a3c
Nélson Campos <nelson.acampos@gmail.com>
2025-04-01 19:29:14 +0100

    app.vue placeholder

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello, Nuxt!</h1>
+    <p>The environment is ready and configured!</p>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const message = 'Welcome to your configured Nuxt application!';
+</script>
+
+<style>
+h1 {
+  color: #42b983;
+}
+</style>
